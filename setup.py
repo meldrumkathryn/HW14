e8aeb4188a6bad7f38db1a79d697d96b7ea682c0
meldrumkathryn <97808270+meldrumkathryn@users.noreply.github.com>
2022-07-11 13:15:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='lovers',
       version='0.1',
       description='a suite of booklover database tools',
-      url='https://github.com/meldrumkathryn/HW14/tree/main/Booklover',
+      url='https://github.com/meldrumkathryn/HW14/tree/main/lovers',
       author='Kate Meldrum',
       author_email='kmm4ap@gmail.com',
       license='MIT',
