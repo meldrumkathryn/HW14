46a153497486092397d711399302faa7bd231539
meldrumkathryn <97808270+meldrumkathryn@users.noreply.github.com>
2022-07-11 12:05:23 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='booklover',
+      version='0.1',
+      description='a suite of booklover database tools',
+      url='',
+      author='Kate Meldrum',
+      author_email='kmm4ap@gmail.com',
+      license='MIT',
+      packages=['booklover.py', 'booklover_test.py'])
